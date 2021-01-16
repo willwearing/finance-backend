4c53e892a6f3bdadc7a75ce0cdb9377bc73da142
willwearing <willwearing@gmail.com>
2021-01-16 13:27:17 -0700

    setup for heroku

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
+
 const server = require("./server.js");
 
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
+const secret = process.env.SECRET_THING || "foo";
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`.random);
+console.log(port, secret);
+
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
